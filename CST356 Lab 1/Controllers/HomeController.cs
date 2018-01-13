a0e708595e3846066e0cbc09cc5e09704757c12e
dylanbrandstetter <35245920+dylanbrandstetter@users.noreply.github.com>
2018-01-13 12:37:19 -0800

    Finished lab

---
+++ CST356 Lab 1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CST356_Lab_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Add more information to this about page.";
+            ViewBag.SecondMessage = "Here's another piece of data.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CST356 Lab 1/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CST356_Lab_1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            ViewBag.FirstName = "Dylan";
+            ViewBag.LastName = "Brandstetter";
+            ViewBag.Age = "20";
+            ViewBag.Gender = "Male";
+
+            return View();
+        }
+    }
+}
